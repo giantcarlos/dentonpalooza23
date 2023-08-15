b60f30ec56baebbecdd93b0a34eefa9205879339
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-08-15 18:25:40 -0500

    add main header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          DENTONPALOOZA 2023
-        </p>
+        <p>Dentonpalooza</p>
+        <h2>SATURDAY / NOVEMBER 4, 2023</h2>
+        <hr />
+        <h3>RUBBER GLOVES REAHEARSAL STUDIOS</h3>
       </header>
     </div>
   );
