dc64171ccb4bc5de05bcd2a9c9a8e0fc3fdbc221
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-08-18 17:20:21 -0500

    add osteria sponsor and message

--- src/App.js
+++ src/App.js
@@ -12,14 +12,16 @@ function App() {
       <div className="image-grid">
           <a href='http://recycledbooks.com/' className='image-reverse-gray'><img src={"https://i.imgur.com/6F5XWoE.png"} /></a>
           <a href='https://www.morefuncomicsandgames.com/' className='image-reverse'><img src={"https://i.imgur.com/k8Vheyt.png"} /></a>
-          <a href='https://www.littleguys.com/' className='image-reverse-gray'><img src={"https://i.imgur.com/24seD6Q.png"} /></a>
           <a href='https://orbitpressusa.com/'><img src={"https://i.imgur.com/FzRrdWC.png"} /></a>
+          <a href='https://www.littleguys.com/' className='image-reverse-gray'><img src={"https://i.imgur.com/24seD6Q.png"} /></a>
           <a href='https://www.smilinrickstattoos.com/'><img src={"https://static.wixstatic.com/media/7bb04e_5bff94f3a5ff437a934f80f543555e5f~mv2_d_1842_1200_s_2.png/v1/fill/w_460,h_298,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/smilinrickssign.png"} /></a>
           <a href='https://danssilverleaf.com/'><img src={"https://i.imgur.com/jHoY46F.png"} /></a>
           <a href='https://oakstreetdrafthouse.com/' className='image-reverse'><img src={"https://i.imgur.com/L1Jf7L6.png"} /></a>
           <a href='https://www.eastsidedtx.com/' className='image-reverse'><img src={"https://i.imgur.com/CWrmgQg.jpg"} /></a>
           <a href='https://www.missangelinesdenton.com/' className='image-reverse'><img src={"https://i.imgur.com/GBB5S1l.png"} /></a>
+          <a href='https://www.osteriamuro.com/' className='image-reverse-gray'><img src={"https://static.wixstatic.com/media/e79e3c_e33fc1745ca043869e93a731355ea4af~mv2.png/v1/fill/w_512,h_238,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Osteria%20Il%20Muro%20Logo_horizontal.png"} /></a>
       </div>
+      <div className="message">A big thanks to our sponsors!</div>
       <div className="container">
         <div className="card">
           <div className="card-title">TICKET INFORMATION</div>
