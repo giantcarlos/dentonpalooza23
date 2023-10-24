0aa50043b10a1e85e13ee3038a1e6b1676579444
Gian Taino Carlos <giantainocarlos@gmail.com>
2023-10-24 15:02:06 -0500

    add Denton Record Chronicle article

--- src/App.js
+++ src/App.js
@@ -91,6 +91,16 @@ function App() {
         </div>
         <div className="card">
           <div className="card-title">PRESS RELEASES / BLURBS</div>
+          <a href="https://dentonrc.com/denton_u/things_to_do/what-the-heck-is-dentonpalooza-the-festival-s-history-and-what-to-expect-this-year/article_374d9895-a4fb-5ea6-b670-7f6a12f69446.html?utm_campaign=blox&utm_source=facebook&utm_medium=social&mibextid=S66gvFa" target="_blank"  className="press-card">
+            <div className="press-image">
+              <img src={"/images/tigerhead.jpg"} className="image-gray" alt='image Tigerhead from Denton Record Chronicle' />
+            </div>
+            <div className="press">
+            <div className="card-details">October 24th, 2023</div>
+              <div className="press-title">What the Heck is Dentonpalooza? The Festival's History and What to Expect This Year</div>
+              <div className="card-details">Emmi Tinajero / Denton Record-Chronicle</div>
+            </div>
+          </a>
           <a href="https://daily.bandcamp.com/scene-report/denton-texas-rock-scene-report" target="_blank"  className="press-card">
             <div className="press-image">
               <img src={"/images/bandcamp.jpeg"} className="image-gray" alt='Denton art piece by A. Savage' />
